c6998a0e22e560963c109b8640286c61edb31bca
lucasdelatorre <luksdlt92@gmail.com>
2017-12-26 00:14:11 -0300

    Kickoff

---
+++ src/main/kotlin/com/github/athenaengine/core/Core.kt
@@ -0,0 +1,9 @@
+package com.github.athenaengine.core
+
+class Core {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            println("Athena Engine - Core")
+        }
+    }
+}
